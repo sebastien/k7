119b698bdf5552445fae5e0dbdab798c58d15125
Sebastien Pierre <sebastien@type-z.org>
2009-05-07 19:17:42 -0400

    Renamed the module

---
+++ tests/module-core-concurrency-libtask-yield.js
@@ -0,0 +1,48 @@
+/**
+Demonstrates the use of the 'libtask.yield' function
+--
+Task 1:0
+Task 2:0
+Task 1:1
+Task 2:1
+Task 1:2
+Task 2:2
+Task 1:3
+Task 2:3
+Task 1:4
+Task 2:4
+Task 1:5
+Task 2:5
+Task 1:6
+Task 2:6
+Task 1:7
+Task 2:7
+Task 1:8
+Task 2:8
+Task 1:9
+Task 2:9
+--
+*/
+
+var libtask = core.concurrency.libtask
+
+function task_1 () {
+	for (var i=0;i<10;i++) {
+		print("Task 1:" + i + "\n")
+		libtask.yield()
+	}
+}
+
+function task_2 () {
+	for (var i=0;i<10;i++) {
+		print("Task 2:" + i + "\n")
+		libtask.yield()
+	}
+}
+
+function main () {
+	libtask.create(task_1,[])
+	libtask.create(task_2,[])
+}
+
+main()
