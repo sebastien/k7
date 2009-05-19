e5687b59891726e1f39c7a5aa7486bab115c9d40
Sebastien Pierre <sebastien@type-z.org>
2009-05-19 10:59:48 -0400

    Minor style update when integrating libevent module

--- lib/system/event/event.cpp
+++ lib/system/event/event.cpp
@@ -1,11 +1,10 @@
 // ----------------------------------------------------------------------------
-// Project		   : K7 - Standard Library for V8
-//                   libevent bindings
-// -----------------------------------------------------------------------------
-// Author           :  Victor Grishchenko       <victor.grishchenko@gmail.com>
+// Project           : K7 - Standard Library for V8
 // ----------------------------------------------------------------------------
-// Creation date     : 8-May-2009
-// Last modification : 16-May-2009
+// Author            : Victor Grishchenko        <victor.grishchenko@gmail.com>
+// ----------------------------------------------------------------------------
+// Creation date     : 08-May-2009
+// Last modification : 19-May-2009
 // ----------------------------------------------------------------------------
 
 #ifdef WITH_EVENT
@@ -19,90 +18,90 @@
 #include <event2/util.h>
 
 using namespace v8;
+
 #define MODULE_NAME   "system.event"
 #define MODULE_STATIC  system_event
 
-
 void evbuf_free(Persistent<Value> watcher, void* parameter) {
-    evbuffer_free((evbuffer*)parameter);
-    watcher.Dispose();
+	evbuffer_free((evbuffer*)parameter);
+	watcher.Dispose();
 }
 
 FUNCTION(evbuf_new) {
-    evbuffer* buf = evbuffer_new();
-    RETURN_WATCHED(buf,evbuf_free);
+	evbuffer* buf = evbuffer_new();
+	RETURN_WATCHED(buf,evbuf_free);
 } END
 
 FUNCTION(evbuf_add8,PWRAP(evbuffer,buf),PINT(i)) {
-    uint8_t i8 = i;
-    evbuffer_add(buf,&i8,1);
-    RETURN_UNDEF;
+	uint8_t i8 = i;
+	evbuffer_add(buf,&i8,1);
+	RETURN_UNDEF;
 } END
 
 FUNCTION(evbuf_add16,PWRAP(evbuffer,buf),PINT(i)) {
-    uint16_t i16 = htons(i);
-    evbuffer_add(buf,&i16,2);
-    RETURN_UNDEF;
+	uint16_t i16 = htons(i);
+	evbuffer_add(buf,&i16,2);
+	RETURN_UNDEF;
 } END
 
 FUNCTION(evbuf_add32,PWRAP(evbuffer,buf),PINT(i)) {
-    uint32_t i32 = htonl(i);
-    evbuffer_add(buf,&i32,4);
-    RETURN_UNDEF;
+	uint32_t i32 = htonl(i);
+	evbuffer_add(buf,&i32,4);
+	RETURN_UNDEF;
 } END
 
 FUNCTION(evbuf_add,PWRAP(evbuffer,buf),PSTR(str)) {
-    evbuffer_add(buf,*str,str.length());
-    RETURN_UNDEF;
+	evbuffer_add(buf,*str,str.length());
+	RETURN_UNDEF;
 } END
 
 FUNCTION(evbuf_remove8,PWRAP(evbuffer,buf)) {
-    uint8_t byte;
-    if (1!=evbuffer_remove(buf,&byte,1))
-        RETURN_UNDEF;
-    else
-        RETURN_INT(byte);
+	uint8_t byte;
+	if (1!=evbuffer_remove(buf,&byte,1))
+		RETURN_UNDEF;
+	else
+		RETURN_INT(byte);
 } END
 
 FUNCTION(evbuf_remove16,PWRAP(evbuffer,buf)) {
-    uint16_t i;
-    if (1!=evbuffer_remove(buf,&i,2))
-        RETURN_UNDEF;
-    else
-        RETURN_INT(ntohs(i));
+	uint16_t i;
+	if (1!=evbuffer_remove(buf,&i,2))
+		RETURN_UNDEF;
+	else
+		RETURN_INT(ntohs(i));
 } END
 
 FUNCTION(evbuf_remove32,PWRAP(evbuffer,buf)) {
-    uint32_t i;
-    if (1!=evbuffer_remove(buf,&i,1))
-        RETURN_UNDEF;
-    else
-        RETURN_INT(htonl(i));
+	uint32_t i;
+	if (1!=evbuffer_remove(buf,&i,1))
+		RETURN_UNDEF;
+	else
+		RETURN_INT(htonl(i));
 } END
 
 FUNCTION(evbuf_remove,PWRAP(evbuffer,buf)) {
-    size_t len = evbuffer_get_length(buf);
-    char* b = (char*)evbuffer_pullup(buf,len);
-    Handle<String> ret = String::New(b,len);
-    evbuffer_drain(buf,len);
-    RETURN_SCOPED(ret);
+	size_t len = evbuffer_get_length(buf);
+	char* b = (char*)evbuffer_pullup(buf,len);
+	Handle<String> ret = String::New(b,len);
+	evbuffer_drain(buf,len);
+	RETURN_SCOPED(ret);
 } END
 
 FUNCTION(evbuf_readln,PWRAP(evbuffer,buf)) {
-    char* str = evbuffer_readln(buf,NULL,EVBUFFER_EOL_CRLF);
-    Handle<String> retstr(String::New(str));
-    free(str);
-    RETURN_SCOPED(retstr);
+	char* str = evbuffer_readln(buf,NULL,EVBUFFER_EOL_CRLF);
+	Handle<String> retstr(String::New(str));
+	free(str);
+	RETURN_SCOPED(retstr);
 } END
-    
+	
 FUNCTION(evbuf_read,PWRAP(evbuffer,buf),PINT(file_des),PINT(max_bytes)) {
-    size_t justread = evbuffer_read(buf,file_des,max_bytes);
-    RETURN_INT(justread);
+	size_t justread = evbuffer_read(buf,file_des,max_bytes);
+	RETURN_INT(justread);
 } END
 
 FUNCTION(evbuf_write,PWRAP(evbuffer,buf),PINT(file_des)) {
-    size_t written = evbuffer_write(buf,file_des);
-    RETURN_INT(written);
+	size_t written = evbuffer_write(buf,file_des);
+	RETURN_INT(written);
 } END
 
 struct _ev_memo_t {
@@ -114,77 +113,77 @@ struct _ev_memo_t {
 event_base * _base = NULL;
 
 void _ev_cb (evutil_socket_t fd, short what, void *arg) {
-    HandleScope scope;
-    struct _ev_memo_t * memo = (struct _ev_memo_t *) arg;
-    Handle<Value> arr [] = {JS_int(fd),JS_int(what)};
-    TryCatch tc;
-    Handle<Value> ret = memo->func->Call(memo->obj,2,arr);
-    if (tc.HasCaught()) {
-        v8::String::AsciiValue exception(tc.Exception());
-        fprintf(stderr,"Uncaught exception: %s\n",*exception);
-    }
-    if (tc.HasCaught() || !ret->IsBoolean() || !ret->ToBoolean()->Value()) {
-        event_del(&(memo->e));
-        memo->obj.Dispose();
-        memo->func.Dispose();
-    }
+	HandleScope scope;
+	struct _ev_memo_t * memo = (struct _ev_memo_t *) arg;
+	Handle<Value> arr [] = {JS_int(fd),JS_int(what)};
+	TryCatch tc;
+	Handle<Value> ret = memo->func->Call(memo->obj,2,arr);
+	if (tc.HasCaught()) {
+		v8::String::AsciiValue exception(tc.Exception());
+		fprintf(stderr,"Uncaught exception: %s\n",*exception);
+	}
+	if (tc.HasCaught() || !ret->IsBoolean() || !ret->ToBoolean()->Value()) {
+		event_del(&(memo->e));
+		memo->obj.Dispose();
+		memo->func.Dispose();
+	}
 }
 
 FUNCTION(le_event_add,PINT(fd),PINT(flags),POBJ(cbthis),PFUN(cbfun)) {
-    _ev_memo_t* memo = (_ev_memo_t*)malloc(sizeof(_ev_memo_t));
-    memo->obj = Persistent<Object>::New(cbthis);
-    memo->func = Persistent<Function>::New(cbfun);
-    event_assign(&(memo->e),_base,fd,flags|EV_PERSIST,_ev_cb,memo);
-    event_add(&(memo->e),NULL);
-    printf("event added fd %i\n",fd);
-    RETURN_WRAPPED(memo);
+	_ev_memo_t* memo = (_ev_memo_t*)malloc(sizeof(_ev_memo_t));
+	memo->obj = Persistent<Object>::New(cbthis);
+	memo->func = Persistent<Function>::New(cbfun);
+	event_assign(&(memo->e),_base,fd,flags|EV_PERSIST,_ev_cb,memo);
+	event_add(&(memo->e),NULL);
+	printf("event added fd %i\n",fd);
+	RETURN_WRAPPED(memo);
 } END
 
 FUNCTION(le_event_del,PWRAP(_ev_memo_t,memo)) {
-    event_del(&(memo->e));
-    memo->obj.Dispose();
-    memo->func.Dispose();
-    free(memo);
+	event_del(&(memo->e));
+	memo->obj.Dispose();
+	memo->func.Dispose();
+	free(memo);
 } END
 
 FUNCTION(le_event_loop,PINT(millis)) {
-    struct timeval delay;
-    delay.tv_sec = millis/1000;
-    delay.tv_usec = (millis%1000)*1000;
-    //event_base_loopexit(_base,&delay);
-    event_base_loop(_base,0);
+	struct timeval delay;
+	delay.tv_sec = millis/1000;
+	delay.tv_usec = (millis%1000)*1000;
+	//event_base_loopexit(_base,&delay);
+	event_base_loop(_base,0);
 } END
 
 FUNCTION(le_make_socket_nonblocking,PINT(sock)) {
-    int ret = evutil_make_socket_nonblocking(sock);
-    RETURN_INT(ret);
+	int ret = evutil_make_socket_nonblocking(sock);
+	RETURN_INT(ret);
 } END
 
 MODULE {
-    BIND("buffer_new",evbuf_new);
-    //DESTRUCTOR(evbuf_destructor)
-    BIND("buffer_add8",evbuf_add8);
-    BIND("buffer_add16",evbuf_add16);
-    BIND("buffer_add32",evbuf_add32);
-    BIND("buffer_add",evbuf_add);
-    BIND("buffer_remove8",evbuf_remove8);
-    BIND("buffer_remove16",evbuf_remove16);
-    BIND("buffer_remove32",evbuf_remove32);
-    BIND("buffer_remove",evbuf_remove);
-    BIND("buffer_read",evbuf_read);
-    BIND("buffer_write",evbuf_write);
-    BIND("buffer_readln",evbuf_readln);
-    BIND("event_add",le_event_add);
-    BIND("event_del",le_event_del);
-    BIND("event_loop",le_event_loop);
-    BIND("make_socket_nonblocking",le_make_socket_nonblocking);
-    SET_int("EV_TIMEOUT",EV_TIMEOUT);
-    SET_int("EV_READ",EV_READ);
-    SET_int("EV_WRITE",EV_WRITE);
-    SET_int("EV_SIGNAL",EV_SIGNAL);
-    struct event_config *cfg = event_config_new();
-    event_config_require_features(cfg,EV_FEATURE_FDS);
-    _base = event_base_new_with_config(cfg);
+	BIND("buffer_new",evbuf_new);
+	//DESTRUCTOR(evbuf_destructor)
+	BIND("buffer_add8",evbuf_add8);
+	BIND("buffer_add16",evbuf_add16);
+	BIND("buffer_add32",evbuf_add32);
+	BIND("buffer_add",evbuf_add);
+	BIND("buffer_remove8",evbuf_remove8);
+	BIND("buffer_remove16",evbuf_remove16);
+	BIND("buffer_remove32",evbuf_remove32);
+	BIND("buffer_remove",evbuf_remove);
+	BIND("buffer_read",evbuf_read);
+	BIND("buffer_write",evbuf_write);
+	BIND("buffer_readln",evbuf_readln);
+	BIND("event_add",le_event_add);
+	BIND("event_del",le_event_del);
+	BIND("event_loop",le_event_loop);
+	BIND("make_socket_nonblocking",le_make_socket_nonblocking);
+	SET_int("EV_TIMEOUT",EV_TIMEOUT);
+	SET_int("EV_READ",EV_READ);
+	SET_int("EV_WRITE",EV_WRITE);
+	SET_int("EV_SIGNAL",EV_SIGNAL);
+	struct event_config *cfg = event_config_new();
+	event_config_require_features(cfg,EV_FEATURE_FDS);
+	_base = event_base_new_with_config(cfg);
 } END_MODULE
 
 #endif
--- src/macros.h
+++ src/macros.h
@@ -1,12 +1,14 @@
-// ----------------------------------------------------------------------------
-// Project		   : K7 - Standard Library for V8
 // -----------------------------------------------------------------------------
-// Authors			  Sebastien Pierre				   <sebastien@type-z.org>
-//                    Victor Grishchenko       <victor.grishchenko@gmail.com>
+// Project           : K7 - Standard Library for V8
+// -----------------------------------------------------------------------------
+// Author            : Sebastien Pierre                   <sebastien@type-z.org>
+//                     Victor Grishchenko         <victor.grishchenko@gmail.com>
 // ----------------------------------------------------------------------------
 // Creation date     : 27-Sep-2008
-// Last modification : 16-May-2009
+// Last modification : 19-May-2009
 // ----------------------------------------------------------------------------
+
+
 
 #ifndef __K7_MACROS__
 #define __K7_MACROS__
@@ -36,8 +38,8 @@ using namespace v8;
 #define JS_obj(o)                      v8::Object::New(o)
 #define JS_bool(b)                     v8::Boolean::New(b)
 
-#define JS_THROW(t, s)                   ThrowException(Exception::t(String::New(s)))
-#define JS_ERROR(s)                      ThrowException(Exception::Error(String::New(s)))
+#define JS_THROW(t, s)                 ThrowException(Exception::t(String::New(s)))
+#define JS_ERROR(s)                    ThrowException(Exception::Error(String::New(s)))
 
 // ----------------------------------------------------------------------------
 //
@@ -49,47 +51,47 @@ using namespace v8;
  * This set of macro make it easy to declare a function that can be exported to
  * the JavaScript environment.
  * See the 'posix.cpp' module for examples. */
-#define ARGC						   args.Length()
-#define ARG_COUNT(c)				   if ( args.Length() != c ) { \
-									   return ThrowException(String::New("Insufficient arguments")); } 
-#define ARG_BETWEEN(a,b)			   if ( a <= args.Length() <= b ) {} 
-#define ARG_int(n,c)				   int n=(int)(args[c]->Int32Value())
-#define ARG_str(v,i)				   v8::String::AsciiValue v(args[i]);
-#define ARG_utf8(v,i)				  v8::String::Utf8Value  v(args[i])
-#define ARG_obj(v,i)				   v8::Local<v8::Object> v=args[i]->ToObject();
-#define ARG_obj(v,i)				   v8::Local<v8::Object> v=args[i]->ToObject();
+#define ARGC                           args.Length()
+#define ARG_COUNT(c)                   if ( args.Length() != c ) { \
+                                       return ThrowException(String::New("Insufficient arguments")); } 
+#define ARG_BETWEEN(a,b)               if ( a <= args.Length() <= b ) {} 
+#define ARG_int(n,c)                   int n=(int)(args[c]->Int32Value())
+#define ARG_str(v,i)                   v8::String::AsciiValue v(args[i]);
+#define ARG_utf8(v,i)                  v8::String::Utf8Value  v(args[i])
+#define ARG_obj(v,i)                   v8::Local<v8::Object> v=args[i]->ToObject();
+#define ARG_obj(v,i)                   v8::Local<v8::Object> v=args[i]->ToObject();
 #define ARG_array(name, c) \
-		if (!args[(c)]->IsArray()) { \
-			//std::ostringstream __k7_e; \
-			//__k7_e << "Exception: argument error." << __func__ << " expects array for argument " << c << "."; \
-			//return ThrowException(String::New(__k7_e.str().c_str())); \
-		} \
-		Handle<Array> name = Handle<Array>::Cast(args[(c)])
+        if (!args[(c)]->IsArray()) { \
+            //std::ostringstream __k7_e; \
+            //__k7_e << "Exception: argument error." << __func__ << " expects array for argument " << c << "."; \
+            //return ThrowException(String::New(__k7_e.str().c_str())); \
+        } \
+        Handle<Array> name = Handle<Array>::Cast(args[(c)])
 #define ARG_fn(name, c) \
-		Handle<Function> name = Handle<Function>::Cast(args[(c)])
+        Handle<Function> name = Handle<Function>::Cast(args[(c)])
 
 #define THROW(str)              return ThrowException(String::New(str))
 #define THROW_VERB(tmpl,...)    { char msg[1024]; snprintf(msg,1000,tmpl,__VA_ARGS__); THROW(msg); }
 // performance freaks use this THROW_VERB :)
 //#define THROW_VERB(tmpl,...)  THROW("Invocation error")
+
 #define FUN_NAME            (*String::AsciiValue(args.Callee()->GetName()->ToString()))
 
-#define PINT(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsInt32()) \
-								THROW_VERB("Argument %i of %s, must be an integer\n",_argn,FUN_NAME); \
-							int n=(int)(args[_argn-1]->Int32Value()); 0
-#define PSTR(n)             0; if (args.Length()<++_argn) \
-								THROW_VERB("Argument %i of %s must be a string\n",_argn,FUN_NAME); \
-							v8::String::AsciiValue n(args[_argn-1]); 0
-#define POBJ(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsObject()) \
-								THROW_VERB("Argument %i of %s, must be an object\n",_argn,FUN_NAME); \
-							v8::Handle<v8::Object> n (args[_argn-1]->ToObject()); 0
-#define PFUN(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsFunction()) \
-								THROW_VERB("Argument %i of %s, must be a function\n",_argn,FUN_NAME);  \
-							v8::Handle<v8::Function> n (v8::Function::Cast(*args[_argn-1])); 0
-#define PWRAP(type,var)   0; if (args.Length()<++_argn || !args[_argn-1]->IsExternal()) \
-                                THROW_VERB("Argument %i of %s, must be an external\n",_argn,FUN_NAME);  \
+#define PINT(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsInt32()) {\
+                                THROW_VERB("Argument %i of %s, must be an integer\n",_argn,FUN_NAME);} \
+                            int n=(int)(args[_argn-1]->Int32Value()); 0
+#define PSTR(n)             0; if (args.Length()<++_argn) {\
+                                THROW_VERB("Argument %i of %s must be a string\n",_argn,FUN_NAME);} \
+                            v8::String::AsciiValue n(args[_argn-1]); 0
+#define POBJ(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsObject()) {\
+                                THROW_VERB("Argument %i of %s, must be an object\n",_argn,FUN_NAME);} \
+                            v8::Handle<v8::Object> n (args[_argn-1]->ToObject()); 0
+#define PFUN(n)             0; if (args.Length()<++_argn || !args[_argn-1]->IsFunction()) {\
+                                THROW_VERB("Argument %i of %s, must be a function\n",_argn,FUN_NAME);} \
+                            v8::Handle<v8::Function> n (v8::Function::Cast(*args[_argn-1])); 0
+#define PWRAP(type,var)   0; if (args.Length()<++_argn || !args[_argn-1]->IsExternal()) {\
+                                THROW_VERB("Argument %i of %s, must be an external\n",_argn,FUN_NAME);} \
                             type* var = (type*) v8::External::Cast(*args[_argn-1])->Value(); 0
-
 
 // ----------------------------------------------------------------------------
 //
@@ -97,19 +99,19 @@ using namespace v8;
 //
 // ----------------------------------------------------------------------------
 
-#define FUNCTION_DECL(f)			   static v8::Handle<v8::Value> f(const v8::Arguments&);
-#define FUNCTION(f,...)        	       static v8::Handle<v8::Value> f(const v8::Arguments& args) { \
-											v8::HandleScope handlescope; \
-                                            int _argn=0;    \
-                                            __VA_ARGS__;
-#define FUNCTION_C(f)				  static v8::Handle<v8::Value> f(const v8::Arguments& args) {
-#define END							}
-#define THIS						   args.This()
-#define STUB							return ThrowException(Exception::Error(String::New("Stub - Function not implemented")));
-#define SET_INTERNAL(ptr)				args.This()->SetInternalField(0, External::New((void*)ptr));
+#define FUNCTION_DECL(f)        static v8::Handle<v8::Value> f(const v8::Arguments&);
+#define FUNCTION(f,...)         static v8::Handle<v8::Value> f(const v8::Arguments& args) { \
+                                    v8::HandleScope handlescope; \
+                                    int _argn=0;    \
+                                    __VA_ARGS__;
+#define FUNCTION_C(f)           static v8::Handle<v8::Value> f(const v8::Arguments& args) {
+#define END                     }
+#define THIS                    args.This()
+#define STUB                    return ThrowException(Exception::Error(String::New("Stub - Function not implemented")));
+#define SET_INTERNAL(ptr)       args.This()->SetInternalField(0, External::New((void*)ptr));
 #define GET_INTERNAL(type,var)  Local<Value> _intfld = args.This()->GetInternalField(0); \
-								type var = reinterpret_cast<type>(Handle<External>::Cast(_intfld)->Value());
-#define RETURN_SCOPED(x)		return handlescope.Close(x)
+                                type var = reinterpret_cast<type>(Handle<External>::Cast(_intfld)->Value());
+#define RETURN_SCOPED(x)        return handlescope.Close(x)
 #define RETURN_INT(i)           return handlescope.Close(Integer::New(i))
 #define RETURN_WRAPPED(ptr)     return v8::External::New(ptr)
 #define RETURN_UNDEF            return JS_undefined
@@ -130,20 +132,20 @@ using namespace v8;
 
 #define OBJECT(name,fields,...) \
 v8::Handle<Object> name(__VA_ARGS__) { \
-	v8::HandleScope handle_scope;\
-	v8::Handle<v8::FunctionTemplate>   __class__  = v8::FunctionTemplate::New();\
-	v8::Handle<v8::ObjectTemplate>     __object__ = __class__->InstanceTemplate();\
-	__object__->SetInternalFieldCount(fields);\
-	v8::Handle<v8::Object>               self = __object__->NewInstance();
+    v8::HandleScope handle_scope;\
+    v8::Handle<v8::FunctionTemplate>   __class__  = v8::FunctionTemplate::New();\
+    v8::Handle<v8::ObjectTemplate>     __object__ = __class__->InstanceTemplate();\
+    __object__->SetInternalFieldCount(fields);\
+    v8::Handle<v8::Object>               self = __object__->NewInstance();
 
 #define OBJECT_COPY_SLOTS(from,to) \
 { \
-	Handle<Array> keys = from->GetPropertyNames(); \
-	for (int i = 0; i < keys->Length(); i ++) { \
-		Handle<String> key = keys->Get(JS_int(i))->ToString(); \
-		Handle<Value> val = from->Get(key); \
-		to->Set(key, val); \
-	} \
+    Handle<Array> keys = from->GetPropertyNames(); \
+    for (int i = 0; i < keys->Length(); i ++) { \
+        Handle<String> key = keys->Get(JS_int(i))->ToString(); \
+        Handle<Value> val = from->Get(key); \
+        to->Set(key, val); \
+    } \
 }
 
 #define OBJECT_SET(o,s,v)   o->Set(JS_str(s), v)
@@ -153,10 +155,10 @@ v8::Handle<Object> name(__VA_ARGS__) { \
 
 
 #define INTERNAL(i,value) \
-	self->SetInternalField(i, v8::External::New((void*)value));
+    self->SetInternalField(i, v8::External::New((void*)value));
 
 #define EXTERNAL(type,name,object,indice) \
-	type name = (type) (v8::Local<v8::External>::Cast(object->GetInternalField(indice))->Value());
+    type name = (type) (v8::Local<v8::External>::Cast(object->GetInternalField(indice))->Value());
 
 // ----------------------------------------------------------------------------
 //
@@ -165,18 +167,18 @@ v8::Handle<Object> name(__VA_ARGS__) { \
 // ----------------------------------------------------------------------------
 
 #define CLASS(name)\
-	{ \
-		v8::Handle<v8::String>			__class_name__ = v8::String::New(name); \
-		v8::Handle<v8::FunctionTemplate>  __class__	  = v8::FunctionTemplate::New(); \
-		v8::Handle<v8::ObjectTemplate>	__object__	 = __class__->InstanceTemplate(); \
-		v8::Handle<v8::ObjectTemplate>	self		   = __object__; \
-		__class__->SetClassName(v8::String::New(name)); 
-#define CONSTRUCTOR(c)	   __class__->SetCallHandler(c);
-#define DESTRUCTOR(d)		Persistent<Object> _weak_handle = Persistent<Object>::New(__object__); \
-							 _weak_handle.MakeWeak(NULL, d);
+    { \
+        v8::Handle<v8::String>            __class_name__ = v8::String::New(name); \
+        v8::Handle<v8::FunctionTemplate>  __class__      = v8::FunctionTemplate::New(); \
+        v8::Handle<v8::ObjectTemplate>    __object__     = __class__->InstanceTemplate(); \
+        v8::Handle<v8::ObjectTemplate>    self           = __object__; \
+        class__->SetClassName(v8::String::New(name)); 
+#define CONSTRUCTOR(c)       __class__->SetCallHandler(c);
+#define DESTRUCTOR(d)        Persistent<Object> _weak_handle = Persistent<Object>::New(__object__); \
+                             _weak_handle.MakeWeak(NULL, d);
 #define INTERNAL_FIELDS(i)   __object__->SetInternalFieldCount(i);
-#define HAS_INTERNAL		 __object__->SetInternalFieldCount(1);
-#define END_CLASS			__module__->Set(__class_name__,__class__->GetFunction(),v8::PropertyAttribute(v8::ReadOnly|v8::DontDelete));}
+#define HAS_INTERNAL         __object__->SetInternalFieldCount(1);
+#define END_CLASS            __module__->Set(__class_name__,__class__->GetFunction(),v8::PropertyAttribute(v8::ReadOnly|v8::DontDelete));}
 
 // ----------------------------------------------------------------------------
 //
@@ -185,15 +187,15 @@ v8::Handle<Object> name(__VA_ARGS__) { \
 // ----------------------------------------------------------------------------
 
 #define MODULE\
-	extern "C" v8::Handle<v8::Object> MODULE_STATIC (v8::Handle<v8::Object> __module__) {\
-		v8::Handle<v8::Object> self = __module__;
+    extern "C" v8::Handle<v8::Object> MODULE_STATIC (v8::Handle<v8::Object> __module__) {\
+        v8::Handle<v8::Object> self = __module__;
 #ifdef AS_PLUGIN
-	#define END_MODULE \
-	return __module__; } \
-	extern "C" void k7_module_init () {printf("Loading %s\n", MODULE_NAME)}
+    #define END_MODULE \
+    return __module__; } \
+    extern "C" void k7_module_init () {printf("Loading %s\n", MODULE_NAME)}
 #else
-	#define END_MODULE \
-	return __module__; }
+    #define END_MODULE \
+    return __module__; }
 #endif
 
 // ----------------------------------------------------------------------------
@@ -202,19 +204,20 @@ v8::Handle<Object> name(__VA_ARGS__) { \
 //
 // ----------------------------------------------------------------------------
 
-#define WITH_CLASS		{ v8::Handle<v8::Object> self = __class__;
-#define WITH_MODULE	   { v8::Handle<v8::Object> self = __module__;
-#define WITH_OBJECT	   { v8::Handle<v8::Object> self = __object__;
+#define WITH_CLASS        { v8::Handle<v8::Object> self = __class__;
+#define WITH_MODULE       { v8::Handle<v8::Object> self = __module__;
+#define WITH_OBJECT       { v8::Handle<v8::Object> self = __object__;
 
-#define SET(s,v)		  self->Set(JS_str(s),v);
-#define SET_int(s,v)	  self->Set(JS_str(s), JS_int(v));
-#define SET_str(s,v)	  self->Set(JS_str(s), JS_str(v));
+#define SET(s,v)          self->Set(JS_str(s),v);
+#define SET_int(s,v)      self->Set(JS_str(s), JS_int(v));
+#define SET_str(s,v)      self->Set(JS_str(s), JS_str(v));
 
-#define BIND(s,v)		 {  Handle<Function> f = v8::FunctionTemplate::New(v)->GetFunction(); \
+#define BIND(s,v)        {  Handle<Function> f = v8::FunctionTemplate::New(v)->GetFunction(); \
                             f->SetName(JS_str(s));  \
-                            self->Set(JS_str(s),f); }
-#define METHOD(s,v)	   __object__->Set(JS_str(s),v8::FunctionTemplate::New(v)->GetFunction());
-#define BIND_CONST(s,v)	 self->Set(JS_str(s),v);
+                            self->Set(JS_str(s),f); \
+                         }
+#define METHOD(s,v)       __object__->Set(JS_str(s),v8::FunctionTemplate::New(v)->GetFunction());
+#define BIND_CONST(s,v)   self->Set(JS_str(s),v);
 #define CLASS_METHOD(s,v) __class__->Set(JS_str(s),v8::FunctionTemplate::New(v)->GetFunction());
 
 // ----------------------------------------------------------------------------
@@ -233,4 +236,4 @@ v8::Handle<Object> name(__VA_ARGS__) { \
 #define EVAL(source)                k7::eval(source);
 
 #endif
-// EOF - vim: ts=4 sw=4 noet
+// EOF - vim: ts=4 sw=4 et
